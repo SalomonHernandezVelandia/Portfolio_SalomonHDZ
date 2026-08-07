ca50864f07512257f9831dc85e6c18d75f45d996
SalomonHernandezVelandia <salomon.hdz.dev@gmail.com>
2026-08-06 22:26:20 -0500

    Responsive del card completado

---
+++ src/js/components/panels.js
@@ -0,0 +1,39 @@
+const overlay=document.querySelector(".mobile-overlay");
+
+const buttons=document.querySelectorAll("[data-open]");
+
+const closes=document.querySelectorAll(".close-panel");
+
+buttons.forEach(button=>{
+
+button.addEventListener("click",()=>{
+
+if(window.innerWidth>1024) return;
+
+const id=button.dataset.open;
+
+document.getElementById(id).classList.add("show");
+
+overlay.classList.add("show");
+
+});
+
+});
+
+closes.forEach(btn=>{
+
+btn.addEventListener("click",closePanels);
+
+});
+
+overlay.addEventListener("click",closePanels);
+
+function closePanels(){
+
+document.querySelectorAll(".mobile-panel")
+
+.forEach(panel=>panel.classList.remove("show"));
+
+overlay.classList.remove("show");
+
+}
